9b8470251d545cb3f173f323ef7b170e946298d6
daggtes <valttesfaye@gmail.com>
2022-03-17 13:25:12 -0700

    1 commit

---
+++ Dagggg.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ class Dagggg {
+
+  public static void main(String[] args)
+  {
+    Scanner input = new Scanner (System.in);
+    System.out.print("Enter your first name: ");
+    String firstname = input.next();
+    System.out.print("Enter your last name: ");
+    String lastname = input.next();
+    
+    System.out.print("Your full name is : " +" "+ firstname+ " "+ lastname );
+  }
+} 
